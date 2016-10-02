bf5a1ff234f031e4f3278ccc406fa58763b7233c
Mark Hatchell <mark.hatchell@prospect33.com>
2016-10-02 19:07:23 -0400

    made registration work and added validators. Added authentication status aware nav bar

--- src/app/Account/account.module.ts
+++ src/app/Account/account.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RegisterComponent } from './components/register.component';
 import { APP_PROVIDERS } from '../app.providers';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { LoadingModule } from '../directives/Loading/loading.module';
 
 @NgModule({
@@ -12,6 +12,7 @@ import { LoadingModule } from '../directives/Loading/loading.module';
     imports: [
         CommonModule,
         FormsModule,
+        ReactiveFormsModule,
         LoadingModule
     ],
     exports: [
--- src/app/Account/components/register.component.ts
+++ src/app/Account/components/register.component.ts
@@ -1,45 +1,64 @@
 import { Component } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { Router } from '@angular/router';
-import { NewUser } from '../models/new-user';
+import { FormGroup, Validators, FormControl, FormBuilder } from '@angular/forms';
+import { ValidationService } from '../../Validators/ValidationService';
+import { User } from '../models/user';
 
 @Component({
     selector: 'as-register',
     templateUrl: 'app/Account/templates/register.html',
     styleUrls: [
         'app/Account/styles/register.css'
-    ]
+    ],
+    providers: [FormBuilder]
 })
 
 export class RegisterComponent {
 
-    public user: NewUser = new NewUser({});
+    public user: User;
 
-    public errorMsg: string | void;
+    public errors: Object;
 
+    private registered: boolean = false;
+
+    private form: FormGroup;
     private loading: boolean = false;
 
     constructor(private _userService: UserService,
-                private _router: Router) {
-        return;
+                private _router: Router,
+                private fb: FormBuilder
+    ) {
+        this.buildForm();
     };
 
+    buildForm() {
+        this.form = new FormGroup({
+            first_name: new FormControl('', Validators.required),
+            last_name: new FormControl('', Validators.required),
+            email: new FormControl('', Validators.compose([Validators.required, ValidationService.emailValidator])),
+            username: new FormControl('', Validators.compose([Validators.required, Validators.minLength(2)])),
+            password: new FormControl('', Validators.compose([ValidationService.passwordValidator, ValidationService.validateControl('password_again')])),
+            password_again: new FormControl('', Validators.compose([ValidationService.passwordValidator, ValidationService.matchesFieldValidator('password')]))
+        });
+
+    }
+
     registerUser() {
-        this.loading = true;
-        this.errorMsg = null;
-        console.log('registering');
-        this._userService.register(JSON.stringify(this.user))
-            .subscribe((res) => {
-                    this.loading = false;
-                },
-                (errorMsg) => {
-                    this.loading = false;
-                    if (errorMsg.hasOwnProperty('non_field_errors')) {
-                        this.errorMsg = errorMsg.non_field_errors;
+        if (this.form.valid) {
+            this.loading = true;
+            this.errors = undefined;
+            this._userService.register(JSON.stringify(this.form.getRawValue()))
+                .subscribe((res) => {
+                        this.loading = false;
+                        this.registered = true;
+                    },
+                    (errors) => {
+                        this.loading = false;
+                        this.errors = errors;
                     }
-                    this.user.password = '';
-                }
-            );
+                );
+        }
     }
 
 }
