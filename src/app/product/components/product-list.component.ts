85d4c2599f1c5d7eb92b4b30844081995415ed97
Mark Hatchell <mark.hatchell@prospect33.com>
2016-10-08 17:50:04 -0400

    added the category filter on product listing

--- src/app/product/components/product-list.component.ts
+++ src/app/product/components/product-list.component.ts
@@ -17,7 +17,9 @@ export class ProductListComponent implements OnInit {
     public productsResponse: ListResponse;
     public categoryResponse: ListResponse;
     public product: Product;
-    private loading: boolean = true;
+    public currentCategory;
+    private loadingProducts: boolean = true;
+    private loadingCategories: boolean = true;
     constructor (
         private _productService: ProductService,
         private _categoryService: CategoryService,
@@ -26,19 +28,22 @@ export class ProductListComponent implements OnInit {
         }
 
     ngOnInit() {
+        this.getCategories();
         this.getProducts();
     }
 
     getCategories() {
+        this.loadingCategories = true;
         this._categoryService.getList().subscribe((res) => {
             this.categoryResponse = res;
-            this.loading = false;
+            this.loadingCategories = false;
         });
     }
     getProducts() {
+        this.loadingProducts = true;
         this._productService.getList().subscribe((res) => {
             this.productsResponse = res;
-            this.loading = false;
+            this.loadingProducts = false;
         });
     }
     addToCart(product: Product) {
@@ -46,10 +51,37 @@ export class ProductListComponent implements OnInit {
             //
         });
     }
-    search(item: string) {
-        this._productService.getList({search: item}).subscribe((res) => {
+    search(searchTerm: string) {
+        this.loadingProducts = true;
+        let params = {};
+        if (typeof this.currentCategory !== 'undefined') {
+            let field = 'category';
+            params[field] = this.currentCategory.id;
+        }
+        if (searchTerm !== '') {
+            let field = 'search';
+            params[field] = searchTerm;
+        }
+        this._productService.getList(params).subscribe((res) => {
             this.productsResponse = res;
-            this.loading = false;
+            this.loadingProducts = false;
         });
     }
+
+    searchKeyPressed($event) {
+        if ($event.keyCode === 13) {
+            this.search($event.target.value);
+        }
+    }
+
+    activateCategory(cat) {
+        if (cat === null) {
+            this.currentCategory = undefined;
+            this.search(this.searchterm);
+        } else {
+            this.currentCategory = cat;
+            this.search(this.searchterm);
+        }
+    }
+
 }
