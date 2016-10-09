ecaa6513e83376420c0a95e403d56ba10b587862
Mark Hatchell <mark.hatchell@prospect33.com>
2016-10-09 00:15:48 -0400

    made categories nestable.

--- src/app/product/models/category.ts
+++ src/app/product/models/category.ts
@@ -5,12 +5,20 @@ export class Category extends BaseModel {
 
     public id: number;
     public title: string;
+    public categories: Category[] = [];
 
  constructor (obj: Object) {
         super();
         for (let field in obj) {
             if (obj.hasOwnProperty(field)) {
-                this[field] = obj[field];
+                if (field === 'categories') {
+                    this.categories = [];
+                    for (let cat of obj[field]) {
+                        this.categories.push(new Category(cat));
+                    }
+                } else {
+                    this[field] = obj[field];
+                }
             }
         }
     }
