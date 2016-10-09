d20cc071c8fce6c47c8498cc4c84feb47c1a3d19
Mark Hatchell <mark.hatchell@prospect33.com>
2016-10-08 23:42:05 -0400

    fixed home route

--- src/app/home/home.routes.ts
+++ src/app/home/home.routes.ts
@@ -3,5 +3,5 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
 
 export const HomeRoutes: Routes = [
-  { path: '/home',  component: HomeComponent }
+  { path: 'home',  component: HomeComponent }
 ];
