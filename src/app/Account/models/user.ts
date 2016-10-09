21c5347d872146ace78e315155b3fada22f1fcc8
Mark Hatchell <mark.hatchell@prospect33.com>
2016-10-09 01:03:20 -0400

    made admins be able to change title and description of product.

--- src/app/Account/models/user.ts
+++ src/app/Account/models/user.ts
@@ -4,20 +4,20 @@ export class User extends BaseModel {
 
     id: number;
     last_login: string | Date;
-    is_superuser: boolean;
     username: string;
     first_name: string;
     last_name: string;
     email: string;
     is_staff: boolean;
     is_active: boolean;
+    is_superuser: boolean;
+    is_admin: boolean;
     date_joined: string | Date;
     full_name: string;
     mobile_number: string;
     default_billing_address: number;
     default_shipping_address: number;
     mpesa_id: string;
-    is_admin: boolean;
     avatar: string;
     groups: any[];
     user_permissions: any[];
--- src/app/product/components/product-detail.component.ts
+++ src/app/product/components/product-detail.component.ts
@@ -4,6 +4,7 @@ import { Product } from '../models/product';
 import { ActivatedRoute } from '@angular/router';
 import { CartService } from '../../cart/services/cart.service';
 import { ToasterService } from 'angular2-toaster';
+import { SessionService } from '../../services/SessionService';
 
 interface Attributes {
     size?: number;
@@ -22,13 +23,16 @@ interface RouteParams {
 })
 export class ProductDetailComponent implements OnInit {
     public product: Product;
+    public productUpdates: any = {};
     public loading: boolean = true;
     public chosen_attributes: Attributes = {};
+    public showSave: boolean = false;
 
     constructor(
         private _productService: ProductService,
         private _activatedRoute: ActivatedRoute,
         private _cart: CartService,
+        private _sessionService: SessionService,
         private _toasterService: ToasterService
 
     ) {
@@ -53,5 +57,42 @@ export class ProductDetailComponent implements OnInit {
         this._cart.add(product.id, this.chosen_attributes).subscribe((res) => {
             this._toasterService.pop('success', 'Added To Cart', product.title);
         });
+    }
+
+    productChanged($event, field) {
+
+        if (this._sessionService.user !== null) {
+            if (this._sessionService.user.is_admin ||
+                this._sessionService.user.is_staff ||
+                this._sessionService.user.is_superuser
+            ) {
+                this.showSave = true;
+                // console.log(field, $event.target.innerHTML);
+                this.productUpdates[field] = $event.target.innerHTML;
+            }
+        }
+    }
+
+    saveChanges() {
+        if (this._sessionService.user !== null) {
+            if (this._sessionService.user.is_admin ||
+                this._sessionService.user.is_staff ||
+                this._sessionService.user.is_superuser
+            ) {
+                this.loading = true;
+                let prod = new Product(this.product);
+                for (let field in this.productUpdates) {
+                    if (this.productUpdates.hasOwnProperty(field)) {
+                        prod[field] = this.productUpdates[field].replace(/style=".*?"/ig, '');
+                    }
+                }
+                this._productService.put(prod.id, JSON.stringify(prod)).subscribe((res) => {
+                    this.loading = false;
+                    this.product = res;
+                    this.showSave = false;
+                    this._toasterService.pop('success', 'Saved changes', this.product.title);
+                });
+            }
+        }
     }
 }
