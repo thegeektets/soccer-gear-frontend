5c60da7cbe23d94a4c9e4f43d182e75d88b9ef4a
Mark Hatchell <mark.hatchell@prospect33.com>
2016-10-08 23:36:44 -0400

    changed home page route

--- src/app/product/product.routes.ts
+++ src/app/product/product.routes.ts
@@ -4,6 +4,7 @@ import { ProductListComponent } from './components/product-list.component';
 
 
 export const ProductRoutes: Routes = [
+    { path: '',  component: ProductListComponent },
     { path: 'products',  component: ProductListComponent },
     { path: 'product/:id',  component: ProductDetailComponent }
 ];
