aee29749240d2017dae659b5296ddfb39e3a2ff5
Griffin Muteti <griffin@tunaweza.com>
2016-10-17 13:13:00 +0300

    Issue #27 add new order  after payment request

--- src/app/checkout/models/order.ts
+++ src/app/checkout/models/order.ts
@@ -1,12 +1,30 @@
 import {BaseModel} from '../../bases/models/BaseModel';
+import {Payment} from '../../checkout/models/payment';
+import { User } from '../../Account/models/user';
 
 export class Order extends BaseModel {
 
-    public id: number;
+    public user: User;
     public status: string ;
-    public payment: string;
+    public payment: Payment;
     public cost: string ;
-    public user_id: number;
+    public user_id: any;
+
+    constructor (obj: Object) {
+        super();
+        for (let field in obj) {
+            if (obj.hasOwnProperty(field)) {
+                this[field] = obj[field];
+            }
+        }
+    }
+}
+export class NewOrder extends BaseModel {
+
+    public user: User;
+    public status: string ;
+    public cost: string ;
+    public user_id: any;
 
     constructor (obj: Object) {
         super();
