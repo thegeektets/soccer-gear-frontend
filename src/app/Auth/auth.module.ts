cf0f3ac796ffd9714c7188d103111d2b9025da50
Stella Sikhila <stella@tunaweza.com>
2016-10-17 17:38:29 +0300

    #23 Forgot password user interface and Component

--- src/app/Auth/auth.module.ts
+++ src/app/Auth/auth.module.ts
@@ -4,15 +4,19 @@ import { APP_PROVIDERS } from '../app.providers';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { LoadingModule } from '../directives/Loading/loading.module';
+import {PasswordRecoveryComponent} from './components/passwordRecovery.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
     declarations: [
-        AuthComponent
+        AuthComponent,
+        PasswordRecoveryComponent
     ],
     imports: [
         CommonModule,
         FormsModule,
-        LoadingModule
+        LoadingModule,
+        RouterModule
     ],
     exports: [
         AuthComponent
