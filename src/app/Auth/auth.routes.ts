d0805e8cc0d7688700e136d9518b19326bd5505f
Griffin Muteti <griffin@tunaweza.com>
2016-10-21 13:23:33 +0300

    Merge branch 'dev' of gitlab.tunaweza.com:tunaweza/soccer-gear-frontend into dev

--- src/app/Auth/auth.routes.ts
+++ src/app/Auth/auth.routes.ts
@@ -1,5 +1,7 @@
 import {AuthComponent} from './components/auth.component';
+import {PasswordRecoveryComponent} from './components/passwordRecovery.component';
 
 export const AuthRoutes = [
-    { path: 'auth/login',  component: AuthComponent }
+    { path: 'auth/login',  component: AuthComponent },
+    { path: 'password/recovery',  component: PasswordRecoveryComponent }
 ];
---
+++ src/app/Auth/components/passwordRecovery.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'as-auth',
+    templateUrl: 'app/Auth/templates/recoverPassword.html',
+    styleUrls: [
+        'app/Auth/styles/auth.css'
+    ]
+})
+
+export class PasswordRecoveryComponent {
+
+}
