6b151fa40fc07b2f411ea23ba9279849e014c7e2
Griffin Muteti <griffin@tunaweza.com>
2016-11-04 10:44:24 +0300

    checkout changes request payment template

--- src/app/admin/components/admin-add-product.component.ts
+++ src/app/admin/components/admin-add-product.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
-
+import { FormGroup, Validators, FormControl, FormBuilder } from '@angular/forms';
 
 @Component({
     selector: 'as-admin-add-product',
     templateUrl: 'app/admin/templates/admin_add_product.html',
     styleUrls: [
         'app/admin/styles/admin_add_product.css'
-    ]
+    ],
+    providers: [FormBuilder]
 })
 
 
@@ -16,7 +16,16 @@ export class AdminAddProductComponent implements OnInit {
     public errors: Object;
     public loading: boolean = true;
     private oid: string;
+    private productForm: FormGroup;
+    constructor(private fb: FormBuilder) {
+        this.buildForm();
+    }
     ngOnInit() {
         // nothing here yet
+    }
+    buildForm() {
+        this.productForm = new FormGroup({
+            productName: new FormControl('', Validators.required),
+        });
     }
 }
