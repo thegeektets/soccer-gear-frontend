237822be82d51ac4506263e298daf4c5ddf0d390
Griffin <griffin@tunaweza.com>
2016-11-07 13:54:53 +0300

    added users list

---
+++ src/app/admin/components/admin-list-payments.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FormGroup, Validators, FormControl, FormBuilder } from '@angular/forms';
+import { Product } from '../../product/models/product';
+import { SessionService } from '../../services/SessionService';
+import { ToasterService } from 'angular2-toaster';
+import { ListResponse } from '../../bases/models/ListResponse';
+import { OrderService } from '../../checkout/services/order.service';
+import { Order } from '../../checkout/models/order';
+
+@Component({
+    selector: 'as-admin-list-payments',
+    templateUrl: 'app/admin/templates/admin_list_payments.html',
+    styleUrls: [
+        'app/admin/styles/admin_list_payments.css'
+    ]
+})
+
+export class AdminListPaymentsComponent implements OnInit {
+    public errors: Object;
+    public loading: boolean = true;
+    public product: Product;
+    public orderResponse: ListResponse;
+    private oid: string;
+    private productForm: FormGroup;
+    constructor(
+        private fb: FormBuilder,
+        private _sessionService: SessionService,
+        private _orderService: OrderService,
+        private _toasterService: ToasterService
+                ) {
+        }
+    ngOnInit() {
+        this.getPayments();
+    }
+
+    getPayments() {
+        this.loading = true;
+        this._orderService.getList().subscribe((res) => {
+            this.orderResponse = res;
+            this.loading = false;
+        });
+    }
+
+
+}
+
