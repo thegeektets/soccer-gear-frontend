21b08692897126de3d63eed758ab91e5378d92b7
Griffin <griffin@tunaweza.com>
2016-11-08 10:36:51 +0300

    remove admin list payments

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,7 +4,6 @@ import { AdminDashboardComponent } from './components/admin-dashboard.component'
 import { AdminAddProductComponent } from './components/admin-add-product.component';
 import { AdminListProductComponent} from './components/admin-list-product.component';
 import { AdminListOrdersComponent} from './components/admin-list-orders.component';
-import { AdminListPaymentsComponent} from './components/admin-list-payments.component';
 import { AdminListUsersComponent} from './components/admin-list-users.component';
 import { LoadingModule } from '../directives/Loading/loading.module';
 import { CommonModule } from '@angular/common';
@@ -20,7 +19,6 @@ import { AdminAddCategoryComponent} from './components/admin_add_category.compon
         AdminAddProductComponent,
         AdminListProductComponent,
         AdminAddCategoryComponent,
-        AdminListPaymentsComponent,
         AdminListOrdersComponent,
         AdminListUsersComponent
     ],
@@ -37,7 +35,6 @@ import { AdminAddCategoryComponent} from './components/admin_add_category.compon
         AdminAddProductComponent,
         AdminListProductComponent,
         AdminAddCategoryComponent,
-        AdminListPaymentsComponent,
         AdminListUsersComponent,
         AdminListOrdersComponent
     ],
--- src/app/admin/admin.routes.ts
+++ src/app/admin/admin.routes.ts
@@ -3,7 +3,6 @@ import { AdminAddProductComponent } from './components/admin-add-product.compone
 import { AdminListProductComponent} from './components/admin-list-product.component';
 import { AdminListOrdersComponent} from './components/admin-list-orders.component';
 import { AdminAddCategoryComponent} from './components/admin_add_category.component';
-import { AdminListPaymentsComponent} from './components/admin-list-payments.component';
 import { AdminListUsersComponent} from './components/admin-list-users.component';
 
 export const AdminRoutes = [
@@ -12,7 +11,6 @@ export const AdminRoutes = [
     { path: 'admin/listproduct',  component: AdminListProductComponent },
     { path: 'admin/addcategory',  component: AdminAddCategoryComponent },
     { path: 'admin/listorders',  component: AdminListOrdersComponent } ,
-    { path: 'admin/listpayments',  component: AdminListPaymentsComponent } ,
     { path: 'admin/listusers',  component: AdminListUsersComponent } ,
 ];
 
