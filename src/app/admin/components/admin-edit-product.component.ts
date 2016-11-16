f749bc8f03989e0515ec9afd1dd7000df6c920cd
stella sikhila <stella@tunaweza.com>
2016-11-16 09:59:25 -0500

    category ui

--- src/app/admin/components/admin-edit-product.component.ts
+++ src/app/admin/components/admin-edit-product.component.ts
@@ -1,19 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
-import { FormGroup, Validators, FormControl, FormBuilder } from '@angular/forms';
-import { ProductService } from '../../product/services/product.service';
-import { CategoryService } from '../../product/services/category.service';
-import { Product } from '../../product/models/product';
-import { SessionService } from '../../services/SessionService';
-import { ToasterService } from 'angular2-toaster';
-import { ListResponse } from '../../bases/models/ListResponse';
+import {FormGroup, Validators, FormControl, FormBuilder} from '@angular/forms';
+import {ProductService} from '../../product/services/product.service';
+import {CategoryService} from '../../product/services/category.service';
+import {Product} from '../../product/models/product';
+import {SessionService} from '../../services/SessionService';
+import {ToasterService} from 'angular2-toaster';
+import {ListResponse} from '../../bases/models/ListResponse';
 import {Category} from '../../product/models/category';
-
 interface RouteParams {
     id: string;
 }
-
-
 @Component({
     selector: 'as-admin-add-product',
     templateUrl: 'app/admin/templates/admin_edit_product.html',
@@ -22,8 +19,6 @@ interface RouteParams {
     ],
     providers: [FormBuilder]
 })
-
-
 export class AdminEditProductComponent implements OnInit {
     public errors: Object;
     public loading: boolean = true;
@@ -35,27 +30,28 @@ export class AdminEditProductComponent implements OnInit {
     private uploadFile: any;
     private imageUploaded: boolean = false;
     private hasBaseDropZoneOver: boolean = false;
-    private options: Object  = {
+    private options: Object = {
         url: 'http://www.localhost:8000/api/v1/fileupload/'
     };
-    constructor(
-        private fb: FormBuilder,
-        private _sessionService: SessionService,
-        private _productService: ProductService,
-        private _categoryService: CategoryService,
-        private _toasterService: ToasterService,
-        private _activatedRoute: ActivatedRoute
-                ) {
-         this._activatedRoute.params.subscribe((res: RouteParams) => {
+
+    constructor(private fb: FormBuilder,
+                private _sessionService: SessionService,
+                private _productService: ProductService,
+                private _categoryService: CategoryService,
+                private _toasterService: ToasterService,
+                private _activatedRoute: ActivatedRoute) {
+        this._activatedRoute.params.subscribe((res: RouteParams) => {
             if (res.hasOwnProperty('id')) {
                 this.getProducts(res.id);
             }
         });
         this.buildForm();
     }
+
     ngOnInit() {
         this.getCategories();
     }
+
     buildForm() {
         this.productForm = new FormGroup({
             title: new FormControl(''),
@@ -65,12 +61,14 @@ export class AdminEditProductComponent implements OnInit {
             images: new FormControl('-'),
         });
     }
+
     getCategories() {
         this._categoryService.getList().subscribe((res) => {
             this.categoryResponse = res;
             this.loadingcats = false;
         });
     }
+
     getProducts(id) {
         this.loading = true;
         this._productService.get(id).subscribe((res) => {
@@ -78,6 +76,7 @@ export class AdminEditProductComponent implements OnInit {
             this.loading = false;
         });
     }
+
     handleUpload(data): void {
         if (data && data.response) {
             data = JSON.parse(data.response);
@@ -89,12 +88,13 @@ export class AdminEditProductComponent implements OnInit {
     fileOverBsase(e: any): void {
         this.hasBaseDropZoneOver = e;
     }
+
     editProduct() {
         this.loading = true;
         let productData = this.productForm.getRawValue();
         if (this.imageUploaded) {
-             productData['datafile'] = this.uploadFile;
-             productData['datafile_id'] = this.uploadFile.id;
+            productData['datafile'] = this.uploadFile;
+            productData['datafile_id'] = this.uploadFile.id;
         } else {
             productData['datafile'] = this.product.datafile;
             productData['datafile_id'] = this.product.datafile.id;
